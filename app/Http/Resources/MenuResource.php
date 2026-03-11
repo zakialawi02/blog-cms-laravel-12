3e49415a9bc744ab285d54a0f6f51b72a40d2c15
Ahmad zaki Alawi <zaki.alawi22@gmail.com>
2026-03-11 22:17:47 +0700

    Feat: Add dynamic menus

---
+++ app/Http/Resources/MenuResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\MenuItemResource;
+
+class MenuResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'location' => $this->location,
+            'items' => MenuItemResource::collection($this->whenLoaded('items')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
